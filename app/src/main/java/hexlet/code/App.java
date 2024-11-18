819ec354502e4fa5468b890426bb3942e228d1a3
prvmjsky <lukyanovalexey1@gmail.com>
2024-11-18 19:05:45 +0500

    set game selection in App
    and rename welcomeUser() to greetUser()

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,19 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.welcomeUser();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        String userChoice = scanner.next();
+
+        if (userChoice.equals("1")) {
+            Cli.greetUser();
+        }
     }
 }
