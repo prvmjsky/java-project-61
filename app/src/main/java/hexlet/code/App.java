9ca52b48b07326317d1cc0a1263151572ab79abc
prvmjsky <lukyanovalexey1@gmail.com>
2024-11-21 02:22:31 +0500

    add Prime game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,6 +5,7 @@ import hexlet.code.games.Even;
 import hexlet.code.games.Calc;
 import hexlet.code.games.GCD;
 import hexlet.code.games.Progression;
+import hexlet.code.games.Prime;
 
 public class App {
     public static void main(String[] args) {
@@ -16,6 +17,7 @@ public class App {
         System.out.println("3 - Calc");
         System.out.println("4 - GCD");
         System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
         var userChoice = scanner.next();
@@ -26,6 +28,7 @@ public class App {
             case "3" -> Calc.play(scanner);
             case "4" -> GCD.play(scanner);
             case "5" -> Progression.play(scanner);
+            case "6" -> Prime.play(scanner);
             default -> scanner.close();
         }
     }
---
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -0,0 +1,38 @@
+package hexlet.code.games;
+
+import java.util.Scanner;
+
+public class Prime {
+    public static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+
+        for (var i = 2; i < number / 2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void play(Scanner scanner) {
+        Engine.greet(scanner);
+
+        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+
+        while (Engine.isContinuing()) {
+            var randomNumber = Engine.getRandomNumber(82); // multiplication table range
+            var correctAnswer = isPrime(randomNumber) ? "yes" : "no";
+
+            System.out.println("Question: " + randomNumber);
+            System.out.print("Your answer: ");
+            var userAnswer = scanner.next();
+
+            Engine.compareAnswers(userAnswer, correctAnswer);
+        }
+
+        Engine.end(scanner);
+    }
+}
