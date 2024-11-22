41ab3b829de5da91f9941cead9ae6a442d1e5f7b
prvmjsky <lukyanovalexey1@gmail.com>
2024-11-23 03:55:03 +0500

    fix isPrime

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -8,7 +8,7 @@ public class Prime {
             return false;
         }
 
-        for (var i = 2; i < number / 2; i++) {
+        for (var i = 2; i <= number / 2; i++) {
             if (number % i == 0) {
                 return false;
             }
